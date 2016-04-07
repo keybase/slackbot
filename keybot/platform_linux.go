45f0d3b1e57f5dedf481495836a05a2898653acf
Jack O'Connor <oconnor663@gmail.com>
2016-04-07 13:16:57 -0400

    make the Linux build show some output on success

--- keybot/platform_linux.go
+++ keybot/platform_linux.go
@@ -14,7 +14,7 @@ func setEnvCommand(name string, val string) slackbot.ExecCommand {
 }
 
 func buildStartCommand() slackbot.ExecCommand {
-	return slackbot.NewExecCommand("bash", []string{"-c", "systemctl --user start keybase.prerelease.service"}, true, "Perform a build")
+	return slackbot.NewExecCommand("bash", []string{"-c", "systemctl --user start keybase.prerelease.service && echo 'SUCCESS'"}, true, "Perform a build")
 }
 
 func buildStopCommand() slackbot.ExecCommand {
