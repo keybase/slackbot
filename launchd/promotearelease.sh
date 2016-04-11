157b19ab5a2c4bfad7e2d7fe6620e287081f492b
Gabriel Handford <gabrielh@gmail.com>
2016-04-11 12:41:42 -0700

    Promote a release plist and script

---
+++ launchd/promotearelease.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$dir"
+
+logpath=${LOG_PATH:-}
+
+client_dir="$GOPATH/src/github.com/keybase/client"
+
+echo "Loading release tool"
+"$client_dir/packaging/goinstall.sh" "github.com/keybase/release"
+release_bin="$GOPATH/bin/release"
+
+err_report() {
+  "$client_dir/packaging/slack/send.sh" "Error see $logpath"
+}
+
+trap 'err_report $LINENO' ERR
+
+"$release_bin" promote-a-release --release="$RELEASE_TO_PROMOTE" --bucket-name="prerelease.keybase.io" --platform="darwin"
