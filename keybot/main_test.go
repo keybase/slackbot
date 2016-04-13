29a9d068aebdeaf3067023cd59cb817768166637
Gabriel Handford <gabrielh@gmail.com>
2016-04-13 13:27:47 -0700

    Add test

--- keybot/main_test.go
+++ keybot/main_test.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/keybase/slackbot"
@@ -33,6 +34,16 @@ func TestPromoteRelease(t *testing.T) {
 		t.Fatal(err)
 	}
 	if out != "Dry Run: Doing that would run `/bin/launchctl` with args: [start keybase.prerelease.promotearelease]" {
+		t.Errorf("Unexpected output: %s", out)
+	}
+}
+
+func TestInvalidUsage(t *testing.T) {
+	out, err := kingpinHandler([]string{"release", "oops"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !strings.HasPrefix(out, "```\nI don't know what you mean by") {
 		t.Errorf("Unexpected output: %s", out)
 	}
 }
