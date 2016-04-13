4686fbacac78ad836c7d821cd078ba4dea8c6618
Gabriel Handford <gabrielh@gmail.com>
2016-04-13 00:45:03 -0700

    Fix test

--- keybot/main_test.go
+++ keybot/main_test.go
@@ -18,9 +18,15 @@ func TestAddCommands(t *testing.T) {
 }
 
 func TestKinpinHandler(t *testing.T) {
-	out, err := kingpinHandler([]string{"build", "please"})
+	out, err := kingpinHandler([]string{"build", "darwin"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("out: %s", out)
+	t.Logf("Output: %s", out)
+
+	out2, err2 := kingpinHandler([]string{"build"})
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+	t.Logf("Usage: %s", out2)
 }
