cf72979729d8e489a214b47073aa31e97bbcbe42
Gabriel Handford <gabrielh@gmail.com>
2016-04-13 13:24:56 -0700

    Fix parse context check

--- keybot/main_test.go
+++ keybot/main_test.go
@@ -17,10 +17,22 @@ func TestAddCommands(t *testing.T) {
 	addCommands(bot)
 }
 
-func TestKinpinHandler(t *testing.T) {
+func TestBuildPlease(t *testing.T) {
 	out, err := kingpinHandler([]string{"build", "please"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("out: %s", out)
+	if out != "Dry Run: Doing that would run `/bin/launchctl` with args: [start keybase.prerelease]" {
+		t.Errorf("Unexpected output: %s", out)
+	}
+}
+
+func TestPromoteRelease(t *testing.T) {
+	out, err := kingpinHandler([]string{"release", "promote", "1.2.3"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if out != "Dry Run: Doing that would run `/bin/launchctl` with args: [start keybase.prerelease.promotearelease]" {
+		t.Errorf("Unexpected output: %s", out)
+	}
 }
