aff30b953635761f8aad1ee95b72cf8a6df07b90
Gabriel Handford <gabrielh@gmail.com>
2016-05-09 15:35:22 -0700

    Use platform env for darwin

--- launchd/broken.sh
+++ launchd/broken.sh
@@ -7,14 +7,13 @@ cd "$dir"
 
 client_dir="$GOPATH/src/github.com/keybase/client"
 bucket_name="prerelease.keybase.io"
-platform="darwin"
 
 echo "Loading release tool"
 "$client_dir/packaging/goinstall.sh" "github.com/keybase/release"
 release_bin="$GOPATH/bin/release"
 
-"$release_bin" broken-release --release="$BROKEN_RELEASE" --bucket-name="$bucket_name" --platform="$platform"
-"$client_dir/packaging/slack/send.sh" "Broken $platform $BROKEN_RELEASE ($bucket_name)"
+"$release_bin" broken-release --release="$BROKEN_RELEASE" --bucket-name="$bucket_name" --platform="$PLATFORM"
+"$client_dir/packaging/slack/send.sh" "Broken $PLATFORM $BROKEN_RELEASE ($bucket_name)"
 
 report=`"$release_bin" updates-report --bucket-name="$bucket_name"`
 "$client_dir/packaging/slack/send.sh" "\`\`\`$report\`\`\`"
