b0d14ea3a8e263d3ab08543e9239e95ef7c1e746
Gabriel Handford <gabrielh@gmail.com>
2016-09-14 18:06:49 -0700

    Use run.sh for smoketest.sh

--- launchd/smoketest.sh
+++ launchd/smoketest.sh
@@ -5,20 +5,11 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-logpath=${LOG_PATH:-}
-
 client_dir="$GOPATH/src/github.com/keybase/client"
 
 echo "Loading release tool"
 "$client_dir/packaging/goinstall.sh" "github.com/keybase/release"
 release_bin="$GOPATH/bin/release"
-
-err_report() {
-  url=`$release_bin save-log --bucket-name=prerelease.keybase.io --path=$logpath --noerr`
-  "$client_dir/packaging/slack/send.sh" "Error see $url"
-}
-
-trap 'err_report $LINENO' ERR
 
 if [ -n "$SMOKETEST_BUILD_A" ] && [ -n "$SMOKETEST_BUILD_PLATFORM" ] && [ -n "$SMOKETEST_BUILD_ENABLE" ]; then
   "$release_bin" set-build-in-testing --build-a="$SMOKETEST_BUILD_A" --platform="$SMOKETEST_PLATFORM" --enable="$SMOKETEST_BUILD_ENABLE"
