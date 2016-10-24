61822b9f55aca8876f3336182be2a5e044ec06ae
Chris Ball <chris@printf.net>
2016-10-24 16:00:25 -0400

    smoketesting: support new max_testers API field (#32)
    * support new max_testers API field
    
    * Remove unnecessary env var checks per Jack

--- launchd/smoketest.sh
+++ launchd/smoketest.sh
@@ -11,9 +11,5 @@ echo "Loading release tool"
 "$client_dir/packaging/goinstall.sh" "github.com/keybase/release"
 release_bin="$GOPATH/bin/release"
 
-if [ -n "$SMOKETEST_BUILD_A" ] && [ -n "$SMOKETEST_PLATFORM" ] && [ -n "$SMOKETEST_BUILD_ENABLE" ]; then
-  "$release_bin" set-build-in-testing --build-a="$SMOKETEST_BUILD_A" --platform="$SMOKETEST_PLATFORM" --enable="$SMOKETEST_BUILD_ENABLE"
-  "$client_dir/packaging/slack/send.sh" "Successfully set enable to $SMOKETEST_BUILD_ENABLE for release $SMOKETEST_BUILD_A."
-else
-  "$client_dir/packaging/slack/send.sh" "Error: Missing environment variables in smoketest command."
-fi
+"$release_bin" set-build-in-testing --build-a="$SMOKETEST_BUILD_A" --platform="$SMOKETEST_PLATFORM" --enable="$SMOKETEST_BUILD_ENABLE --max-testers="$SMOKETEST_MAX_TESTERS"
+"$client_dir/packaging/slack/send.sh" "Successfully set enable to $SMOKETEST_BUILD_ENABLE for release $SMOKETEST_BUILD_A."
