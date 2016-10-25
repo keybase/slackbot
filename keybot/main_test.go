b81202bd5265ae9406c32a9266c3e05d3913a4a3
Gabriel Handford <gabrielh@gmail.com>
2016-10-25 13:59:23 -0700

    Fix tests

--- keybot/main_test.go
+++ keybot/main_test.go
@@ -19,17 +19,17 @@ func TestAddCommands(t *testing.T) {
 }
 
 func TestBuildDarwin(t *testing.T) {
-	out, err := kingpinKeybotHandler("", []string{"build", "darwin"})
+	out, err := jobKeybotHandler("", []string{"build", "darwin"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	if out != "Dry Run: Doing that would run `/bin/launchctl` with args: [start keybase.darwin]" {
+	if out != "I would have run a launchd job (keybase.build.darwin)" {
 		t.Errorf("Unexpected output: %s", out)
 	}
 }
 
 func TestPromoteRelease(t *testing.T) {
-	out, err := kingpinKeybotHandler("", []string{"release", "promote", "1.2.3"})
+	out, err := jobKeybotHandler("", []string{"release", "promote", "1.2.3"})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -39,7 +39,7 @@ func TestPromoteRelease(t *testing.T) {
 }
 
 func TestInvalidUsage(t *testing.T) {
-	out, err := kingpinKeybotHandler("", []string{"release", "oops"})
+	out, err := jobKeybotHandler("", []string{"release", "oops"})
 	if err != nil {
 		t.Fatal(err)
 	}
