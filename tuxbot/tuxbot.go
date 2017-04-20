f51df43a2dcda658a5b8730d24b8c81ca7c36841
Gabriel Handford <gabrielh@gmail.com>
2017-04-20 16:48:50 -0700

    Move tuxbot

---
+++ tuxbot/tuxbot.go
@@ -0,0 +1,81 @@
+// Copyright 2015 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+
+	"github.com/keybase/slackbot"
+	"github.com/keybase/slackbot/cli"
+	"github.com/nlopes/slack"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func linuxBuildFunc(channel string, args []string) (string, error) {
+	currentUser, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	prereleaseScriptPath := filepath.Join(currentUser.HomeDir, "slackbot/systemd/prerelease.sh")
+	prereleaseCmd := exec.Command(prereleaseScriptPath)
+	prereleaseCmd.Stdout = os.Stdout
+	prereleaseCmd.Stderr = os.Stderr
+	err = prereleaseCmd.Run()
+	if err != nil {
+		journal, _ := exec.Command("journalctl", "--since=today", "--user-unit", "keybase.keybot.service").CombinedOutput()
+		api := slack.New(slackbot.GetTokenFromEnv())
+		snippetFile := slack.FileUploadParameters{
+			Channels: []string{channel},
+			Title:    "failed build output",
+			Content:  string(journal),
+		}
+		_, _ = api.UploadFile(snippetFile) // ignore errors here for now
+		return "FAILURE", err
+	}
+	return "SUCCESS", nil
+}
+
+type tuxbot struct{}
+
+func (t *tuxbot) Run(bot slackbot.Bot, channel string, args []string) (string, error) {
+	app := kingpin.New("tuxbot", "Command parser for tuxbot")
+	app.Terminate(nil)
+	stringBuffer := new(bytes.Buffer)
+	app.Writer(stringBuffer)
+
+	build := app.Command("build", "Build things")
+	buildLinux := build.Command("linux", "Start a linux build")
+
+	cmd, usage, err := cli.Parse(app, args, stringBuffer)
+	if usage != "" || err != nil {
+		return usage, err
+	}
+
+	switch cmd {
+	case buildLinux.FullCommand():
+		if bot.Config().DryRun() {
+			return "Dry Run: Doing that would run `prerelease.sh`", nil
+		}
+		if bot.Config().Paused() {
+			return "I'm paused so I can't do that, but I would have run `prerelease.sh`", nil
+		}
+
+		return linuxBuildFunc(channel, args)
+	}
+
+	return cmd, nil
+}
+
+func (t *tuxbot) Help(bot slackbot.Bot) string {
+	out, err := t.Run(bot, "", nil)
+	if err != nil {
+		return fmt.Sprintf("Error getting help: %s", err)
+	}
+	return out
+}
