e50d8250c6aab902eb5d5c0a06a8acab0ee5f819
Gabriel Handford <gabrielh@gmail.com>
2017-07-05 15:16:15 -0700

    Use full path for vendoring to work with go run

--- scripts/send.sh
+++ scripts/send.sh
@@ -5,12 +5,12 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
-"$dir/goinstall.sh" "github.com/keybase/slackbot"
+#"$dir/goinstall.sh" "github.com/keybase/slackbot"
 
 # Outputs to slack if you have slackbot installed and SLACK_TOKEN and
 # SLACK_CHANNEL set, otherwise it does nothing (errors are ignored on purpose).
 
-sender="../send/main.go"
+sender="$dir/../send/main.go"
 
 if [ -f $sender ]; then
   go run $sender -i=1 "$@"
