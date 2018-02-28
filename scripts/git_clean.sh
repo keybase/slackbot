943f3ebbc19f6ea7d1a8b1c98e61515523078d84
chrisnojima <cnojima@keyba.se>
2018-02-28 10:36:27 -0500

    fetch and pull and fix path

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -3,14 +3,20 @@
 # This script cleans the go path, go-ios path, go-android path
 set -e -u -o pipefail
 
-cd "$GOPATH/src/client"
+cd "$GOPATH/src/github.com/keybase/client"
+echo $(git fetch)
 echo $(git clean -f)
 echo $(git checkout master)
+echo $(git pull)
 
-cd "$GOPATH/../go-ios/src/client"
+cd "$GOPATH/../go-ios/src/github.com/keybase/client"
+echo $(git fetch)
 echo $(git clean -f)
 echo $(git checkout master)
+echo $(git pull)
 
-cd "$GOPATH/../go-android/src/client"
+cd "$GOPATH/../go-android/src/github.com/keybase/client"
+echo $(git fetch)
 echo $(git clean -f)
 echo $(git checkout master)
+echo $(git pull)
