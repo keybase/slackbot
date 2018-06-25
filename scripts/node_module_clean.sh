83ff5249ee7a542151b12a9270aa889b68184fdf
chrisnojima <chrisnojima@gmail.com>
2018-06-25 10:08:51 -0400

    node modules clean command (#55)

---
+++ scripts/node_module_clean.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# This script cleans the go-ios node_modules 
+set -e -u -o pipefail
+
+rm -rf "$GOPATH/../go-ios/src/github.com/keybase/client/shared/node_modules"
+rm -rf "$GOPATH/../go-android/src/github.com/keybase/client/shared/node_modules"
+cd "$GOPATH/../go-android/src/github.com/keybase/client/shared/node_modules"
+yarn rn-packager-wipe-cache
