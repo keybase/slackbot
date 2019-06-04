e991a409e017eb145a7a094e9b0d6c8519370a4d
Daniel Ayoub <danny.a95@gmail.com>
2019-06-04 09:12:30 -0400

    git clean remove folders too

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -5,18 +5,18 @@ set -e -u -o pipefail
 
 cd "$GOPATH/src/github.com/keybase/client"
 echo $(git fetch)
-echo $(git clean -f)
+echo $(git clean -df)
 echo $(git checkout master)
 echo $(git pull)
 
 cd "$GOPATH/../go-ios/src/github.com/keybase/client"
 echo $(git fetch)
-echo $(git clean -f)
+echo $(git clean -df)
 echo $(git checkout master)
 echo $(git pull)
 
 cd "$GOPATH/../go-android/src/github.com/keybase/client"
 echo $(git fetch)
-echo $(git clean -f)
+echo $(git clean -df)
 echo $(git checkout master)
 echo $(git pull)
