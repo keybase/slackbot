aa2a6003448fd1b78449e1cfcbf21df5dac135c2
Mike Maxim <mike.maxim@gmail.com>
2019-06-05 09:34:21 -0400

    option for no notarize

--- keybot/darwinbot.go
+++ keybot/darwinbot.go
@@ -32,6 +32,7 @@ func (d *darwinbot) Run(bot *slackbot.Bot, channel string, args []string) (strin
 	buildDarwinSkipCI := buildDarwin.Flag("skip-ci", "Whether to skip CI").Bool()
 	buildDarwinSmoke := buildDarwin.Flag("smoke", "Whether to make a pair of builds for smoketesting when on a branch").Bool()
 	buildDarwinNoS3 := buildDarwin.Flag("skip-s3", "Don't push to S3 after building the app").Bool()
+	buildDarwinNoNotarize := buildDarwin.Flag("skip-notarize", "Don't notarize the app").Bool()
 
 	cancel := app.Command("cancel", "Cancel")
 	cancelLabel := cancel.Arg("label", "Launchd job label").Required().String()
@@ -81,6 +82,7 @@ func (d *darwinbot) Run(bot *slackbot.Bot, channel string, args []string) (strin
 				launchd.EnvVar{Key: "NOWAIT", Value: boolToEnvString(skipCI)},
 				launchd.EnvVar{Key: "NOPULL", Value: boolToEnvString(*buildDarwinNoPull)},
 				launchd.EnvVar{Key: "NOS3", Value: boolToEnvString(*buildDarwinNoS3)},
+				launchd.EnvVar{Key: "NONOTARIZE", Value: boolToEnvString(*buildDarwinNoNotarize)},
 			},
 		}
 		// msg := fmt.Sprintf("I'll run the build as job `%s` (skip-ci=%s smoke=%s test=%s).", script.Label, boolToString(skipCI), boolToString(smokeTest), boolToString(testBuild))
