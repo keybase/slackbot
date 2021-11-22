f3d6996dd66dafc69f2296983efb516789444c90
Mike Maxim <mike@keyba.se>
2021-11-22 13:49:45 -0500

    script update

--- scripts/release.broken.sh
+++ scripts/release.broken.sh
@@ -5,8 +5,9 @@ set -e -u -o pipefail # Fail on error
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$dir"
 
+client_dir="$dir/../../client"
 echo "Loading release tool"
-"$dir/goinstall.sh" "github.com/keybase/release"
+(cd "$client_dir/go/buildtools"; go install "github.com/keybase/release")
 release_bin="$GOPATH/bin/release"
 
 "$release_bin" broken-release --release="$BROKEN_RELEASE" --bucket-name="$BUCKET_NAME" --platform="$PLATFORM"
