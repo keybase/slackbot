14b9375326b6e7b77e66e93a98f4ccd7b7d89dc2
chrisnojima <cnojima@keyba.se>
2024-01-09 11:34:19 -0500

    more size in log dump (#94)

--- scripts/dumplog.sh
+++ scripts/dumplog.sh
@@ -2,13 +2,16 @@
 
 set -e -u -o pipefail # Fail on error
 
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$dir"
 
 client_dir="$dir/../../client"
 echo "Loading release tool"
-(cd "$client_dir/go/buildtools"; go install "github.com/keybase/client/go/release")
+(
+	cd "$client_dir/go/buildtools"
+	go install "github.com/keybase/client/go/release"
+)
 release_bin="$GOPATH/bin/release"
 
-url=`"$release_bin" save-log --bucket-name=$BUCKET_NAME --path="$READ_PATH"`
+url=$("$release_bin" save-log --maxsize=5000000 --bucket-name=$BUCKET_NAME --path="$READ_PATH")
 "$dir/send.sh" "Log saved to $url"
