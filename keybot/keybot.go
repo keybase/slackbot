c4fa2da13b05cf05d0663c39c2051d7cbea43eab
chrisnojima <cnojima@keyba.se>
2025-07-16 10:39:06 -0400

    comment out new ver

--- keybot/keybot.go
+++ keybot/keybot.go
@@ -93,7 +93,7 @@ func (k *keybot) Run(bot *slackbot.Bot, channel string, args []string) (string, 
 	env := launchd.NewEnv(home, path)
 	androidHome := "/usr/local/opt/android-sdk"
 	ndkVer65x := "23.1.7779620"
-	ndkVer66x := "26.1.10909125"
+	// ndkVer66x := "26.1.10909125"
 	ndkVer := ndkVer65x 
 	NDKPath := "/Users/build/Library/Android/sdk/ndk/" + ndkVer
 
