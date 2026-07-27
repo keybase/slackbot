4e928f47ce65d7b596c7f3256893777508b8ef14
Joshua Blum <joshua.blum@zoom.us>
2026-07-27 12:28:31 -0400

    nuke tuxbot

--- systemd/nightly-stathat-success.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST -d "stat=tuxbot - nightly - success&ezkey=$STATHAT_EZKEY&count=1" https://api.stathat.com/ez
--- systemd/send.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-set -e -u -o pipefail
-
-repo_root="$(git -C "$(dirname "${BASH_SOURCE[0]}")" rev-parse --show-toplevel)"
-exec "$repo_root/scripts/send.sh" "$@"
